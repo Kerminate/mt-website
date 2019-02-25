40dfb270fcb33a8b36a1fa6f371d750ec47d1fc4
Kerminate <kepeilin@outlook.com>
2019-02-25 09:24:57 +0800

    feat: modify searchbar

--- components/public/header/searchbar.vue
+++ components/public/header/searchbar.vue
@@ -9,21 +9,17 @@
           <el-input
             v-model="search"
             placeholder="搜索商家或地点"
+            @focus="focus"
+            @blur="blur"
+            @input="input"
           />
           <button class="el-button el-button--primary"><i class="el-icon-search" /></button>
-          <dl class="hotPlace">
+          <dl v-if="isHotPlace" class="hotPlace">
             <dt>热门搜索</dt>
-            <dd>火锅</dd>
-            <dd>火锅</dd>
-            <dd>火锅</dd>
-            <dd>火锅</dd>
+            <dd v-for="(item, idx) in hotPlace" :key="idx">{{ item }}</dd>
           </dl>
-          <dl class="searchList">
-            <dd>火锅</dd>
-            <dd>火锅</dd>
-            <dd>火锅</dd>
-            <dd>火锅</dd>
-            <dd>火锅</dd>
+          <dl v-if="isSearchList" class="searchList">
+            <dd v-for="(item, idx) in searchList" :key="idx">{{ item }}</dd>
           </dl>
         </div>
         <p class="suggest">
@@ -56,6 +52,30 @@
 export default {
   data() {
     return {
+      search: '',
+      isFocus: false,
+      hotPlace: ['火锅', '火锅', '火锅', '火锅'],
+      searchList: ['故宫', '故宫', '故宫']
+    }
+  },
+  computed: {
+    isHotPlace() {
+      return this.isFocus && !this.search
+    },
+    isSearchList() {
+      return this.isFocus && this.search
+    }
+  },
+  methods: {
+    focus() {
+      this.isFocus = true
+    },
+    blur() {
+      setTimeout(() => {
+        this.isFocus = false
+      }, 200)
+    },
+    input() {
     }
   }
 }
