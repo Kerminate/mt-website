f3d5f5e50057072f59d97b9927204bce83c6d401
Kerminate <kepeilin@outlook.com>
2019-03-03 09:44:49 +0800

    feat: 完成首页所有静态页面

--- pages/index.vue
+++ pages/index.vue
@@ -1,20 +1,30 @@
 <template>
   <div class="page-index">
     <el-row>
-      <el-col :span="5"><emenu /></el-col>
-      <el-col :span="19">2</el-col>
+      <el-col :span="5">
+        <emenu />
+      </el-col>
+      <el-col :span="19">
+        <elife />
+      </el-col>
     </el-row>
-    <el-row :span="24">
-      3
+    <el-row>
+      <el-col :span="24">
+        <Eartistic />
+      </el-col>
     </el-row>
   </div>
 </template>
 
 <script>
 import Emenu from '@/components/index/menu.vue'
+import Elife from '@/components/index/life.vue'
+import Eartistic from '@/components/index/artistic.vue'
 export default {
   components: {
-    Emenu
+    Emenu,
+    Elife,
+    Eartistic
   }
 }
 </script>
